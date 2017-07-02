fe0e918da31fe7f00300179156f71898970aecf0
Otmar Humbel <ohumbel@gmail.com>
2017-07-02 19:02:51 +0200

    Adds a unit test for RecursiveTestSuiteBuilder

---
+++ src/test/java/net/reini/junit/RecursiveTestSuiteBuilderTest.java
@@ -0,0 +1,51 @@
+package net.reini.junit;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import junit.framework.TestSuite;
+import net.reini.junit.pkg1.NetReiniJunitPkg1_FirstTest;
+import net.reini.junit.pkg1.NetReiniJunitPkg1_SecondTest;
+import net.reini.junit.pkg2.NetReiniJunitPkg2Test;
+
+public class RecursiveTestSuiteBuilderTest {
+
+  @Test
+  public void testNonEmptyPackage() throws Exception {
+    // a list of expected test classes, depth first
+    List<Class<?>> testClasses = new ArrayList<>();
+    testClasses.add(NetReiniJunitPkg1_FirstTest.class);
+    testClasses.add(NetReiniJunitPkg1_SecondTest.class);
+    testClasses.add(NetReiniJunitPkg2Test.class);
+    testClasses.add(NetReiniJunitTest.class);
+    testClasses.add(RecursiveTestSuiteBuilderTest.class);
+
+    // build the suite
+    TestSuite rootSuite = new TestSuite("root suite");
+    RecursiveTestSuiteBuilder.build(RecursiveTestSuiteBuilderTest.class, rootSuite);
+
+    // walk the suite, removing every encountered test class
+    walkSuite(rootSuite, testClasses);
+    assertTrue(testClasses.isEmpty());
+  }
+
+
+  private void walkSuite(TestSuite suite, List<Class<?>> testClasses)
+      throws ClassNotFoundException {
+    int testCount = suite.testCount();
+    for (int i = 0; i < testCount; i++) {
+      junit.framework.Test test = suite.testAt(i);
+      if (test instanceof TestSuite) {
+        walkSuite((TestSuite) test, testClasses);
+      } else {
+        Class<?> testClass = Class.forName(test.toString());
+        assertTrue(testClasses.contains(testClass));
+        testClasses.remove(testClass);
+      }
+    }
+  }
+}
---
+++ src/test/java/net/reini/junit/pkg1/NetReiniJunitPkg1_SecondTest.java
@@ -0,0 +1,10 @@
+package net.reini.junit.pkg1;
+
+import org.junit.Test;
+
+public class NetReiniJunitPkg1_SecondTest {
+
+  @Test
+  public void testMe() {}
+
+}
