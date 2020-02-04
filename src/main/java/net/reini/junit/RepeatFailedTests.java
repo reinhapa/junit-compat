b316c21d3474a97536323f947422e55c412a9ab5
Patrick Reinhart <patrick@reini.net>
2020-02-04 08:38:18 +0100

    Adds Jenkins Gradle output parsing

--- src/main/java/net/reini/junit/RepeatFailedTests.java
+++ src/main/java/net/reini/junit/RepeatFailedTests.java
@@ -1,37 +1,29 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2015, 2019 Patrick Reinhart
+ * Copyright (c) 2015, 20120 Patrick Reinhart
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
-/**
- * File Name: RepeatFailedTests.java
- * 
- * Copyright (c) 2014, 2019 Patrick Reinhart, All Rights Reserved.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package net.reini.junit;
 
 import static java.nio.file.Files.isRegularFile;
 import static java.nio.file.Files.newInputStream;
+import static java.util.regex.Pattern.compile;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -47,12 +39,14 @@ import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
 import org.xml.sax.Attributes;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.XMLFilterImpl;
-import org.xml.sax.helpers.XMLReaderFactory;
+import org.xml.sax.helpers.DefaultHandler;
 
 import junit.framework.JUnit4TestAdapter;
 import junit.framework.Test;
@@ -70,6 +64,10 @@ import junit.framework.TestSuite;
  * @author Patrick Reinhart
  */
 public class RepeatFailedTests {
+  private static final Pattern testName = compile("\\[junit\\] Running (.+)$");
+  private static final Pattern testResult =
+      compile("\\[junit\\] Tests run: [0-9]+, Failures: ([0-9]+), Errors: ([0-9]+),");
+  private static final Pattern gradleName = compile("^\\[.+\\] (.+) > .*FAILED$");
 
   /**
    * @return the builded test suite based on the ANT log output containing the failed tests
@@ -83,29 +81,25 @@ public class RepeatFailedTests {
       if (!urlValue.isEmpty()) {
         try (BufferedReader reader =
             new BufferedReader(new InputStreamReader(new URL(urlValue).openStream()))) {
-          Pattern testNamePattern = Pattern.compile("\\[junit\\] Running (.+)$");
-          Pattern testResultPattern = Pattern
-              .compile("\\[junit\\] Tests run: [0-9]+, Failures: ([0-9]+), Errors: ([0-9]+),");
           String line = null;
           while ((line = reader.readLine()) != null) {
-            Matcher nameMatcher = testNamePattern.matcher(line);
+            Matcher nameMatcher = testName.matcher(line);
             if (nameMatcher.find()) {
-              String className = nameMatcher.group(1);
               line = reader.readLine();
               if (line != null) {
-                Matcher resultMatcher = testResultPattern.matcher(line);
+                Matcher resultMatcher = testResult.matcher(line);
                 if (resultMatcher.find()) {
                   String failures = resultMatcher.group(1);
                   String errors = resultMatcher.group(2);
                   if (!"0".equals(errors) || !"0".equals(failures)) {
-                    if (!className.contains("/")) { // filter out jython tests
-                      addTest(processedClasses, logger, suite, className);
-                    } else {
-                      logger.warning(() -> "Skipped Jython test ".concat(className));
-                    }
+                    addNonJythonTest(processedClasses, logger, suite, nameMatcher.group(1));
                   }
                 }
               }
+            }
+            Matcher gradleNameMatcher = gradleName.matcher(line);
+            if (gradleNameMatcher.matches()) {
+              addNonJythonTest(processedClasses, logger, suite, gradleNameMatcher.group(1));
             }
           }
         } catch (Exception e) {
@@ -119,17 +113,26 @@ public class RepeatFailedTests {
         Path suiteFile = Paths.get(suiteFileName);
         if (isRegularFile(suiteFile)) {
           try (InputStream in = newInputStream(suiteFile)) {
-            XMLReader reader = XMLReaderFactory.createXMLReader();
-            reader.setContentHandler(new TestReportFilter(processedClasses, logger, suite));
-            reader.parse(new InputSource(in));
+            SAXParser parser = SAXParserFactory.newInstance().newSAXParser();
+            parser.parse(new InputSource(in),
+                new TestReportFilter(processedClasses, logger, suite));
           }
         }
-      } catch (SAXException | IOException e) {
+      } catch (ParserConfigurationException | SAXException | IOException e) {
         logger.log(Level.SEVERE, e,
             () -> "Error getting data from suite file ".concat(suiteFileName));
       }
     }
     return suite;
+  }
+
+  static void addNonJythonTest(Set<String> processedClasses, Logger logger, TestSuite suite,
+      String className) {
+    if (!className.contains("/")) { // filter out jython tests
+      addTest(processedClasses, logger, suite, className);
+    } else {
+      logger.warning(() -> "Skipped Jython test ".concat(className));
+    }
   }
 
   static void addTest(Set<String> processedClasses, Logger logger, TestSuite suite,
@@ -151,7 +154,7 @@ public class RepeatFailedTests {
     }
   }
 
-  static final class TestReportFilter extends XMLFilterImpl {
+  static final class TestReportFilter extends DefaultHandler {
     private final Set<String> processedClasses;
     private final Logger logger;
     private final TestSuite suite;
